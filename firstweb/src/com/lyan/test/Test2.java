6386e58420106d41ed9b2ba82377d19d32cdf9df
admin <lyan_work@gmail.com>
2013-11-22 17:32:20 +0800

    test2

---
+++ firstweb/src/com/lyan/test/Test2.java
@@ -0,0 +1,12 @@
+package com.lyan.test;
+
+import org.junit.Test;
+
+public class Test2 {
+	@Test
+	public void projTest(){
+		System.out.println("test ...");
+		System.out.println("test ...");
+		System.out.println("test ...");
+	}
+}
